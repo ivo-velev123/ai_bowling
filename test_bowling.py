41ae8c275635715a802138945879ef8631d0d009
JamesLau2001 <james_lau2001@outlook.com>
2026-04-01 12:22:03 +0100

    Write test for scoring a single open frame

--- test_bowling.py
+++ test_bowling.py
@@ -4,3 +4,8 @@ def test_gutter_game():
     """A game where no pins are knocked down should score 0."""
     game = BowlingGame("00 00 00 00 00 00 00 00 00 00")
     assert game.score() == 0
+
+def test_scoring_in_a_single_open_frame():
+    """A game with one frame of 1 and 2, and the rest zeros, should score 3."""
+    game = BowlingGame("12 00 00 00 00 00 00 00 00 00")
+    assert game.score() == 3
